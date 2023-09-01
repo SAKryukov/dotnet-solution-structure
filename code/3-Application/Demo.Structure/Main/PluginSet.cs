cb226da4df7d97e7313818c12901a4a5086e9d42
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-09-01 15:03:24 -0400

    Application: combined plugin types

--- code/3-Application/Demo.Structure/Main/PluginSet.cs
+++ code/3-Application/Demo.Structure/Main/PluginSet.cs
@@ -1,15 +1,26 @@
 namespace SA.Application {
     using INamedPlugin = Semantic.INamedPlugin;
     using IPropertyPlugin = Semantic.IPropertyPlugin;
+    using IUiPlugin = Semantic.IUiPlugin;
     using PropertyPluginLoader = Agnostic.PluginLoader<Semantic.IPropertyPlugin>;
+    using UiPluginFinder = Agnostic.PluginFinder<Semantic.IUiPlugin>;
     using PluginSet = System.Collections.ObjectModel.ObservableCollection<PluginSetElement>;
 
     class PluginSetExtension : PluginSet {
         internal bool Add(string assemblyPath) {
+            PluginSetElement element;
             PropertyPluginLoader loader = new(assemblyPath);
-            if (loader.Instance == null) return false;
-            PluginSetElement element = new() { Plugin = loader.Instance };
-            Items.Add(element);
+            if (loader.Instance == null && loader.Assembly == null)
+                return false;
+            else if (loader.Instance == null && loader.Assembly != null) {
+                UiPluginFinder uiFinder = new(loader.Assembly);
+                if (uiFinder.Instance == null)
+                    return false;
+                element = new() { Plugin = uiFinder.Instance };
+            } else
+                element = new() { Plugin = loader.Instance };
+            if (element != null)
+                Items.Add(element);
             return true;
         } //Add
         internal void RemovePluginAt(int index) {
