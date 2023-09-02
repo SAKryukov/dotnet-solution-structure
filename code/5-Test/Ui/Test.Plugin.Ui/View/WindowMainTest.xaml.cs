e3a9f9f76782eae0937dec48704fb5a47792308a
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-09-01 20:31:41 -0400

    Test: UI test completed

--- code/5-Test/Ui/Test.Plugin.Ui/View/WindowMainTest.xaml.cs
+++ code/5-Test/Ui/Test.Plugin.Ui/View/WindowMainTest.xaml.cs
@@ -1,11 +1,33 @@
 ﻿namespace SA.Test.View {
     using System.Windows;
+    using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
 
     public partial class WindowMainTest : Window {
         
         public WindowMainTest() {
             InitializeComponent();
+            SetupDialogs();
+            menuItemMain.Click += (_, _) => LoadAndExecuteUiPlugin();
+            textBlockCopyright.Text = Agnostic.UI.AdvancedApplicationBase.Current.Copyright;
         } //WindowMainTest
+
+        void LoadAndExecuteUiPlugin() {
+            loadPluginDialog.InitialDirectory = Agnostic.UI.AdvancedApplicationBase.Current.ExecutablePath;
+            if (loadPluginDialog.ShowDialog() != true) return;
+            Agnostic.PluginLoader<Semantic.IUiPlugin> loader = new(loadPluginDialog.FileName);
+            if (loader.Instance == null) return;
+            loader.Instance.Create(this);
+            loader.Instance.Execute();
+            loader.Instance.Destroy();
+            loader.Unload();
+        } //LoadAndExecuteUiPlugin
+
+        void SetupDialogs() {
+            loadPluginDialog.Filter = Main.DefinitionSet.DialogPropertySet.pluginDialogFilter;
+            loadPluginDialog.Title = Main.DefinitionSet.DialogPropertySet.pluginDialogTitle;
+        } //SetupDialogs
+
+        readonly OpenFileDialog loadPluginDialog = new();
 
     } //class WindowMainTest
 
