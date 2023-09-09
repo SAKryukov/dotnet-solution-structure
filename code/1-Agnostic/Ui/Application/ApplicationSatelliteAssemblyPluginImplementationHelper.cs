72a1b869d181186f3a311e4365756aa9945ccb71
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-09-09 00:02:59 -0400

    Plugin, Build: prevented copy of referenced assemblies: Assemblies referenced by Satellite plugin assemblies are copied in culture-depended sub-directories, and are redundant. Copy is prevented via <Private>False</Private

---
+++ code/1-Agnostic/Ui/Application/ApplicationSatelliteAssemblyPluginImplementationHelper.cs
@@ -0,0 +1,24 @@
+namespace SA.Agnostic.UI {
+    using System.Windows;
+    using ResourceFinderDictionary = System.Collections.Generic.Dictionary<string, System.Windows.ResourceDictionary>;
+
+    public class ApplicationSatelliteAssemblyPluginImplementationHelper : ResourceFinderDictionary {
+        public ApplicationSatelliteAssemblyPluginImplementationHelper(FrameworkElement[] elements) {
+            foreach (var element in elements) {
+                if (element == null) continue;
+                string key = element.GetType().FullName;
+                if (ContainsKey(key)) continue;
+                Add(key, element.Resources);
+            } //loop
+        } //ApplicationSatelliteAssemblyPluginImplementation
+        public ResourceDictionary GetResources(string fullTypeName) {
+            if (TryGetValue(fullTypeName, out ResourceDictionary value))
+                return value;
+            else
+                return null;
+        } //GetResources
+    } //class ApplicationSatelliteAssemblyPluginImplementation
+
+}
+
+
