e2d5075f2574b504b4691f7bcdb04c339265fa13
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-09-12 06:30:47 -0400

    Code: XAML: more dead code removed, improved styling

--- code/4-Plugin/Plugin.UI/Localization/ru-RU/Implementation/DefinitionSet.cs
+++
@@ -1,8 +0,0 @@
-namespace SA.Plugin {
-
-    static class DefinitionSet {
-        internal const string pluginName = "Embedded Control";
-        internal const string testExceptionHandlingByHostCommand = "$";
-    } //class DefinitionSet
-
-}
