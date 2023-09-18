43bade4a8ca52c511567c1a4194ca18b6c371986
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-09-18 04:11:01 -0400

    Test: Test.Extensions: Globalization: working prototype complete: This is the way to provide data from ResourceDictionary to code in a globalized way

--- code/5-Test/Ui/Test.MarkupExtensions/Test/Extension.cs
+++ code/5-Test/Ui/Test.MarkupExtensions/Test/Extension.cs
@@ -2,7 +2,8 @@
     using System;
     using System.Windows;
     using System.Windows.Markup;
-    using SetterList = System.Collections.Generic.List<MemberSetter>;
+    using System.Reflection;
+    using InstanceDictionary = System.Collections.Generic.Dictionary<System.Type, object>;
 
     public enum MemberKind { Field, Property }
     public class MemberSetter {
@@ -12,24 +13,74 @@
         public object Value { get; set; }
     } //class MemberSetter
 
-    public class ResourceSource : MarkupExtension, IAddChild {
-        void IAddChild.AddChild(object value) {
-            //Setters.Add((MemberSetter)value);
-        }
-        void IAddChild.AddText(string text) { }
-        public ResourceSource() {
-            Setters = new();
-        }
+    public class EKey : TypeExtension {
         public Type TargetType { get; set; }
-        public ResourceDictionary Setters { get; set; }
         public override object ProvideValue(IServiceProvider serviceProvider) {
-            return Setters;
+            return this;
         } //ResourSet
-    } //class ResourceSource
-
-    internal class ResourceSet {
-        //ResourceDictionary = 
-    } //class ResourceSet
-
+    }
+    public class Member : MarkupExtension {
+        public Type Type { get; set; }
+        public MemberKind MemberKind { get; set; }
+        public string Name { get; set; }
+        public ResourceDictionary Setters { get; set; }
+        public object Value { get; set; }
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        } //ProvideValue
+        public static InstanceDictionary Collect(ResourceDictionary dictionary) {
+            if (dictionary == null) return null;
+            InstanceDictionary instanceDictionary = new();
+            foreach (var key in dictionary.Keys) {
+                object value = dictionary[key];
+                if (value is not Member resourceSource) continue;
+                if (key is not EKey targetKey) continue;
+                Type targetType = targetKey.TargetType;
+                if (!instanceDictionary.TryGetValue(targetType, out object instance)) {
+                    instance = Activator.CreateInstance(targetType);
+                    instanceDictionary.Add(targetType, instance);
+                } //if
+                Assign(resourceSource, targetType, instance);
+            } //keys loop
+            return instanceDictionary;
+        } //Collect
+        public static void Collect(ResourceDictionary dictionary, object instance) {
+            if (dictionary == null) return;
+            Type instanceType = instance.GetType();
+            foreach (var key in dictionary.Keys) {
+                object value = dictionary[key];
+                if (value is not Member resourceSource) continue;
+                if (key is not EKey targetKey) continue;
+                Type targetType = targetKey.TargetType;
+                if (!targetType.IsAssignableTo(instanceType))
+                    continue;
+                Assign(resourceSource, targetType, instance);
+            } //keys loop
+        } //Collect
+        static void Assign(Member resourceSource, Type targetType, object instance) {
+            MemberKind memberKind = resourceSource.MemberKind;
+            string memberName = resourceSource.Name;
+            if (memberName == null) Raise("Member Name cannot be null");
+            object memberValue = resourceSource.Value;
+            if (memberValue == null) Raise("Member Value cannot be null");
+            Type memberType = resourceSource.Type; 
+            if (memberType == null)
+                memberType = typeof(string);
+            if (memberValue.GetType() != memberType)
+                memberValue = Convert.ChangeType(memberValue, memberType);
+            if (memberKind == MemberKind.Field) {
+                FieldInfo field = targetType.GetField(memberName);
+                if (field == null) Raise($"Field {memberName} cannot be null");
+                field.SetValue(instance, memberValue);
+            } else {
+                PropertyInfo property = targetType.GetProperty(memberName);
+                if (property == null) Raise($"Property {memberName} cannot be null");
+                property.SetValue(instance, memberValue);
+            } //if
+        } //Assign
+        static void Raise(string message) {
+            throw new SystemException(message);
+        } //Raise
+    } //class Member
 
 }
--- code/5-Test/Ui/Test.MarkupExtensions/Test/Test.Extensions.EntryPoint.cs
+++ code/5-Test/Ui/Test.MarkupExtensions/Test/Test.Extensions.EntryPoint.cs
@@ -1,32 +1,21 @@
 namespace SA.Test.Extensions {
     using Console = System.Console;
-    using System;
     using System.Windows;
-    using ResourceDictionarySet = System.Collections.Generic.HashSet<System.Windows.ResourceDictionary>;
-    using SnapshotDictionary = System.Collections.Generic.Dictionary<System.Windows.FrameworkElement, System.Windows.ResourceDictionary>;
-    using FrameworkElementCollection = System.Collections.Generic.IEnumerable<System.Windows.FrameworkElement>;
-    using System.Windows.Markup;
+    using Declarations;
 
+    internal class ResourceTarget {
+        public string Name = null;
+        public string Description { get; set; }
+        public int Count { get; set; }
+    } //class ResourceTarget
 
     static class Test {
 
-        /*
-        static void Raise(string message) {
-            throw new SystemException(message);
-        }
-        */
-
         static void Execute() {
-            //ResourceSource source = new();
-            //ResourceDictionary dictionary = source.Resources;
-            //foreach (var key in dictionary.Keys) {
-                //object value = dictionary[key];
-                //if (value is not Declarations.ResourceSource resourceSource) continue;
-                //foreach(var setter in resourceSource.Setters) {
-                    //if (setter.MemberType == null)
-                    //    Raise("Setter cannot be null");
-                //} //setters loop
-            //} //keys loop
+            ResourceSource source = new();
+            ResourceDictionary dictionary = source.Resources;
+            ResourceTarget target = new();
+            Member.Collect(dictionary, target);
         } //Execute
 
         [System.STAThread]
