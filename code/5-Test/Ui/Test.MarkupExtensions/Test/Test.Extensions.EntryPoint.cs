5a12f5b253a75318a5e72868a2f301aa3371f681
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-09-18 18:33:30 -0400

    Test: markup extensions: moving around more

--- code/5-Test/Ui/Test.MarkupExtensions/Test/Test.Extensions.EntryPoint.cs
+++ code/5-Test/Ui/Test.MarkupExtensions/Test/Test.Extensions.EntryPoint.cs
@@ -1,22 +1,6 @@
 ﻿namespace SA.Test.Extensions {
     using Console = System.Console;
     using System.Windows;
-    using System.Windows.Media;
-
-    internal class ResourceTarget {
-        public Thickness Thickness { get; set; }
-        public Color Color { get; set; }
-        public string Name = null;
-        public string Description { get; set; }
-        public int Count { get; set; }
-    } //class ResourceTarget
-    
-    internal class ResourceTarget2 {
-        public Thickness Thickness2 = default;
-        public string Name2 = null;
-        public string Description2 { get; set; }
-        public int Count2 { get; set; }
-    } //class ResourceTarget
 
     static class Test {
 
---
+++ code/5-Test/Ui/Test.MarkupExtensions/Test/TestTypes.cs
@@ -0,0 +1,22 @@
+﻿namespace SA.Test.Extensions {
+    using System.Windows;
+    //using Type = System.Type;
+    using System.Windows.Media;
+
+    internal class ResourceTarget {
+        public Thickness Thickness { get; set; }
+        public Color Color { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Count = default;
+    } //class ResourceTarget
+
+    internal class ResourceTarget2 {
+        public Thickness Thickness2 = default;
+        public string Name2 { get; set; }
+        public string Description2 { get; set; }
+        readonly int count2 = default;
+        public int PublicCount => count2;
+    } //class ResourceTarget
+
+}
