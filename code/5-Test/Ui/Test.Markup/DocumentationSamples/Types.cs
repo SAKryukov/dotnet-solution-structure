29382790e3f5e3b12d75b6a695725b67485c9a1d
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-09-22 12:24:24 -0400

    Test: Markup: added duck-typing demo of Member.Type

--- code/5-Test/Ui/Test.Markup/DocumentationSamples/Types.cs
+++ code/5-Test/Ui/Test.Markup/DocumentationSamples/Types.cs
@@ -3,6 +3,7 @@
     class DuckTypedSample {
         internal string NationalBird { get; set; }
         public static string NationalTree = null;
+        internal int WordHeritageSites { get; set; }
         public string NationalAnimal = null;
         internal string Аnthem { get; set; }
         internal string[] CarMakes { get; set; }
@@ -12,7 +13,8 @@
             (string name, string value) animal = (nameof(NationalAnimal), NationalAnimal);
             (string name, string value) anthem = (nameof(Аnthem), Аnthem);
             (string name, string value) cars = (nameof(CarMakes), DefinitionSet.FormatStrings(CarMakes));
-            return DefinitionSet.Dump(GetType().Name, bird, cars, tree, animal, anthem);
+            (string name, string value) wordHeritageSites = (nameof(WordHeritageSites), WordHeritageSites.ToString());
+            return DefinitionSet.Dump(GetType().Name, bird, cars, tree, animal, wordHeritageSites, anthem);
         } //ToString
     } //class DuckTypedSample
 
