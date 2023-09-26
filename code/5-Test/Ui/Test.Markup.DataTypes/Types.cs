aee0cf87ca05405b298196d1b8f5df6d339015b6
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-09-26 19:21:05 -0400

    Test: Markup: added demonstration of compound types and direct content

--- code/5-Test/Ui/Test.Markup.DataTypes/Types.cs
+++ code/5-Test/Ui/Test.Markup.DataTypes/Types.cs
@@ -8,25 +8,32 @@
 namespace My {
     using Color = System.Windows.Media.Color;
     using ColorList = System.Collections.Generic.List<System.Windows.Media.Color>;
+    using ContentPropertyAttribute = System.Windows.Markup.ContentPropertyAttribute;
 
+    public class DimensionalQuantity {
+        public double Value { get; set; }
+        public string Units { get; set; }
+    } //DimensionalQuantity
+
+    [ContentProperty(nameof(Description))]
     public class Main {
+        public string Description { get; set; }
         public Main() { Flag = new();  }
         public ColorList Flag { get; set; }
         public string Country { get; set; }
         public string Language { get; set; }
         public string Capital { get; set; }
-        public double Area { get; set; }
-        public double PopulationDensity { get; set; }
-        public string AreaUnits { get; set; }
-        public string PopulationDensityUnits { get; set; }
+        public DimensionalQuantity PopulationDensity { get; set; }
+        public DimensionalQuantity Area { get; set; }
         public override string ToString() {
+            (string name, string value) description = (nameof(Description), Description);
             (string name, string value) flag = (nameof(Flag), DefinitionSet.FormatColors(Flag.ToArray()));
             (string name, string value) country = (nameof(Country), Country);
             (string name, string value) language = (nameof(Language), Language);
             (string name, string value) capital = (nameof(Capital), Capital);
-            (string name, string value) area = (nameof(area), Area.ToString() + AreaUnits);
-            (string name, string value) populationDensity = (nameof(PopulationDensity), PopulationDensity.ToString() + PopulationDensityUnits);
-            return DefinitionSet.Dump(GetType().Name, country, language, flag, capital, area, populationDensity);
+            (string name, string value) area = (nameof(Area), Area.Value.ToString() + Area.Units);
+            (string name, string value) populationDensity = (nameof(PopulationDensity), PopulationDensity.Value.ToString() + PopulationDensity.Units);
+            return DefinitionSet.Dump(GetType().Name, description, country, language, flag, capital, area, populationDensity);
         } //ToString
     } //class Main
 
