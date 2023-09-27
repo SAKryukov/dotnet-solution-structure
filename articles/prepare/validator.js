8a6a9a70d3c57591f197df6a6e00b7bf5ba4dcf9
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-09-27 01:42:23 -0400

    Article: validation of hrefs improved

--- articles/prepare/validator.js
+++ articles/prepare/validator.js
@@ -1,7 +1,9 @@
 // To detect invalid anchor targets, add:
 //     <script src="prepare/validator.js"></script>
 //
+
 window.onload = () => {
+
     const errors = [];
     const external = [];
     const anchors = document.getElementsByTagName("a");
@@ -15,17 +17,24 @@ window.onload = () => {
                 errors.push(anchor);    
         } else
             external.push(anchor);
-
     } //loop
+
     if (errors.length > 0) {
-        document.writeln(`<h1>Errors: invalid anchor targets:</h1>`);
-        for(let anchor of errors)
-            document.writeln(`<h3> ${anchor.hash}<dd>Text: "${anchor.textContent}"</dd></h3>`);
+        document.writeln(
+            `<h1>Errors: invalid anchor hrefs:</h1>`);
+        for (let anchor of errors)
+            document.writeln(
+                `<h3> ${anchor.hash}<dd>Text:
+                    "${anchor.textContent}"</dd></h3>`);
     } //if
+
     if (external.length > 0) {
-        document.writeln(`<h1>External anchors:</h1>`);
-        for(let anchor of external)
-            document.writeln(`<a href="${anchor.href}">${anchor.textContent}</a><br/>`);
+        document.writeln(`<h1>External anchors:</h1><ol>`);
+        for (let anchor of external)
+            document.writeln(
+                `<li><a href="${anchor.href}">
+                    ${anchor.textContent}</a></li>`);
+        document.writeln("</ol>");
     } //if
 
 } //window.onload
