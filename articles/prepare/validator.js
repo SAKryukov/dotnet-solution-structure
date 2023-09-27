ac4e368a6c8a28ece57dfb44fc3ff77e0bbac409
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-09-26 23:10:32 -0400

    Article: improved anchor validator

--- articles/prepare/validator.js
+++ articles/prepare/validator.js
@@ -3,21 +3,29 @@
 //
 window.onload = () => {
     const errors = [];
+    const external = [];
     const anchors = document.getElementsByTagName("a");
     for (let anchor of anchors) {
-        if (!anchor.hash) continue;
-        //if (anchor.protocol == "http:" || anchor.protocol == "https:")
-        //    continue;
-        if (anchor.protocol != "file:")
-            continue;
-        element = document.getElementById(anchor.hash.replace("#", ""));
-        if (!element)
-            errors.push(anchor);
+        if (!anchor.textContent) continue;
+        if (!anchor.href) continue;
+        if (anchor.protocol == "file:") {
+            if (!anchor.hash) continue;
+            element = document.getElementById(anchor.hash.replace("#", ""));
+            if (!element)
+                errors.push(anchor);    
+        } else
+            external.push(anchor);
 
     } //loop
     if (errors.length > 0) {
-        document.writeln(`<h1>Errors: invalid anchor targets:<h1>`);
+        document.writeln(`<h1>Errors: invalid anchor targets:</h1>`);
         for(let anchor of errors)
             document.writeln(`<h3> ${anchor.hash}<dd>Text: "${anchor.textContent}"</dd></h3>`);
     } //if
+    if (external.length > 0) {
+        document.writeln(`<h1>External anchors:</h1>`);
+        for(let anchor of external)
+            document.writeln(`<a href="${anchor.href}">${anchor.textContent}</a><br/>`);
+    } //if
+
 } //window.onload
