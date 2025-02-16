bf8481443daa52af2b578deca10cd5a7e2faf721
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-02-16 18:10:57 -0500

    Code: build refactoring: Abstracted out project references using Directory.Build.props in nested directories Completed Dotnet.diagnostic

--- code/5-Test/Dotnet.diagnostic/Test/Test.EntryPoint.cs
+++ code/5-Test/Dotnet.diagnostic/Test/Test.EntryPoint.cs
@@ -1,10 +1,35 @@
 ﻿namespace Diagnostic {
     using Console = System.Console;
 
+    // ...
+
+    abstract class Base {
+        internal virtual void Run() {/* ... */}
+    }
+    class DerivedOne : Base {
+        internal static int[] Values => new int[] { 1, 2, 3 };
+    }
+    class DerivedTwo : Base {
+    }
+
     class Test {
-        [System.STAThread]
+        static void PrefersVar() {
+            var values = DerivedOne.Values;
+            Console.WriteLine(values.GetType().Name);
+            foreach (var value in DerivedOne.Values)
+                Console.Write($"{value} ");
+        }
+        static void NeedsExplicit() {
+            Base instance = new DerivedOne();
+            instance.Run();
+            //...
+            instance = new DerivedTwo();
+            instance.Run();
+        }
         static void Main() {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;            
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            PrefersVar();
+            NeedsExplicit();
         } //Main
     } //class Test
 
