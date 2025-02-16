4d2968c08f75ff98ef3270911e2c31d21498ba37
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-02-16 00:05:52 -0500

    Code: Tests: added diagnostic test

---
+++ code/5-Test/Dotnet.diagnostic/Test/Test.EntryPoint.cs
@@ -0,0 +1,11 @@
+﻿namespace Diagnostic {
+
+    class Test {
+        [System.STAThread]
+        static void Main() {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Test.Execute();
+        } //Main
+    } //class Test
+
+}
